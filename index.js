24527b4f3ae56724d30eba5802afbbf747d0de37
Jon Ramm <jrgbone@gmail.com>
2021-12-21 12:06:22 -0700

    trombone is playable!

--- index.js
+++ index.js
@@ -9,42 +9,42 @@ $(".site").mouseover(()=> {
 function buttonCheck(button) {
     switch (button) {
         case "1":     
-            const bFlat = new Audio('./sounds/kick-bass.mp3');
+            const bFlat = new Audio("../sounds/bFlat.mp3")
             bFlat.play();
         break;
 
         case "2":
-            const c = new Audio('./sounds/snare.mp3');
+            const c = new Audio('../sounds/c.mp3');
             c.play();
         break;
 
         case "3":
-            const d = new Audio('./sounds/crash.mp3');
+            const d = new Audio('../sounds/d.mp3');
             d.play();
         break;
 
         case "4":
-            const eFlat = new Audio('./sounds/tom-1.mp3');
+            const eFlat = new Audio('../sounds/eFlat.mp3');
             eFlat.play();
         break;
 
         case "5":
-            const f = new Audio('./sounds/tom-2.mp3');
+            const f = new Audio('../sounds/f.mp3');
             f.play();
         break;
 
         case "6":
-            const g = new Audio('./sounds/tom-3.mp3');
+            const g = new Audio('../sounds/g.mp3');
             g.play();
         break;
 
         case "7":
-            const a = new Audio('./sounds/tom-4.mp3');
+            const a = new Audio('../sounds/a.mp3');
             a.play();
         break;
 
         case "8":
-            const bFlatHigh = new Audio('./sounds/tom-4.mp3');
+            const bFlatHigh = new Audio('../sounds/bFlatHigh.mp3');
             bFlatHigh.play();
         break;
 
@@ -52,12 +52,12 @@ function buttonCheck(button) {
 }}
 
 document.addEventListener("keydown", (event) => {
-    // buttonCheck(event.key);
+    buttonCheck(event.key);
     buttonAnimation(event.key)
 });
 
 function buttonAnimation(currentKey) {
     const activeBtn = document.querySelector(".note-" + currentKey); 
     activeBtn.classList.add("pressed");
-    setTimeout(() => activeBtn.classList.toggle("pressed"), 50);
+    setTimeout(() => activeBtn.classList.toggle("pressed"), 200);
 }
