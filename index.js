3deefd46934d156e26dab13ab6b687c3085fac98
jonargee19 <jrgbone@gmail.com>
2021-12-20 20:16:43 -0700

    added a pulse effect to navbar

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 
 $(".site").mouseover(()=> {
-    $(".site").slideDown();
+    $(".site").animate({fontSize: "2.1rem"}, "fast").animate({fontSize: "2rem"}, "fast")
 });
+
+// function scroll() {
+//     $(document).scrollTop(800);
+// }
+
+// setTimeout(scroll, 1500)
