96f8cc88f0a9b92962857d493cd5721b194030d6
jonargee19 <jrgbone@gmail.com>
2022-06-25 17:47:35 -0600

    insert icons

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ $(".site").mouseover(()=> {
     $(".site").animate({fontSize: "2.1rem"}, "fast").animate({fontSize: "2rem"}, "fast")
 });
 
-const interests = ["basketball encyclopedia.", "brewery enthusiast.", "jazz lover.", "cat dad.", "dog father.", "cyclist."]
+const interests = [
+                    "basketball encyclopedia.", 
+                    "brewery enthusiast.", 
+                    "jazz lover.", 
+                    "cat dad.",
+                    "burrito connoisseur.", 
+                    "dog father.", 
+                    "cyclist."
+                ]
 const interestsLength = interests.length
 const interestsDiv = document.getElementById("rotatingInterests");
 const inst = setInterval(setInterest, 1500)
