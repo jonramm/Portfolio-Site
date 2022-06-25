bccee68f7ad44d1bcc3075dab0373fd201a86710
jonargee19 <jrgbone@gmail.com>
2022-06-25 15:14:28 -0600

    home header style

--- index.js
+++ index.js
@@ -3,5 +3,16 @@ $(".site").mouseover(()=> {
     $(".site").animate({fontSize: "2.1rem"}, "fast").animate({fontSize: "2rem"}, "fast")
 });
 
+const interests = ["basketball encyclopedia.", "brewery enthusiast.", "jazz lover.", "cat dad.", "dog father.", "cyclist."]
+const interestsLength = interests.length
+const interestsDiv = document.getElementById("rotatingInterests");
+const inst = setInterval(setInterest, 2000)
+
+function setInterest() {
+    const index = (Math.floor((Math.random()*interestsLength)))
+    interestsDiv.innerHTML = interests[index]
+}
+
+
 
 
